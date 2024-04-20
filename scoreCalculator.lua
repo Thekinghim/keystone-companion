1a4cb3ecca78a6de85b1d66d868363f7db0c3083
Larsj02 <l.m.johnen@gmail.com>
2024-04-20 21:56:45 +0200

    Base

---
+++ scoreCalculator.lua
@@ -0,0 +1,7 @@
+local KeystoneCompanion = select(2, ...)
+
+local calculatorFrame = KeystoneCompanion.widgets.RoundedFrame.CreateFrame(UIParent, {
+    height = 400,
+    width = 600,
+    border_size = 2,
+})
