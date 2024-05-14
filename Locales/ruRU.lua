2b613cdf71a294665605f2c634f9a3dbf0a265a5
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-05-15 00:47:39 +0500

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,74 @@
+local _, Private = ...
+Private.Locales = Private.Locales or {}
+
+Private.Locales["ruRU"] = {
+    -- isEditing = true,
+    -- ui/bestDungeons.lua
+    ["WEEKLY BEST"] = "ЛУЧШЕЕ НА ЭТОЙ НЕДЕЛЕ",
+
+    -- ui/dungeonTimer.lua
+    ["M+ Count"] = "М+ Количество",
+
+    -- ui/main.lua
+    ["Player name"] = "Имя игрока",
+    ["Food"] = "Еда",
+    ["Runes"] = "Руны",
+    ["Potions"] = "Зелья",
+    ["Flasks"] = "Фласки",
+    ["Weapon"] = "Оружие",
+    ["Join us on Discord"] = "Присоединяйтесь к нам в Discord",
+    ["Copy below URL into your browser."] = "Скопируйте указанный ниже URL-адрес в браузер.",
+    ["Mythic+ score"] = "Счёт М+",
+    ["Item level"] = "Уровень предмета",
+    ["Item level in bags"] = "Уровень предмета в сумках",
+    ["You do not have this teleport"] = "У тебя нет этого телепорта",
+    ["Teleport not usable"] = "Телепорт не доступен",
+    ["Click to teleport"] = "Нажмите, чтобы телепортироваться",
+
+    -- ui/scoreCalculator.lua
+    ["Target Score"] = "Необходимый счёт",
+    ["Max. Key Level"] = "Макс. уровень ключа",
+    ["Only this Week"] = "Только на этой неделе",
+    ["Calculate"] = "Рассчёт",
+    ["NAME"] = "НАЗВАНИЕ",
+    ["LEVEL"] = "УРОВЕНЬ",
+    ["AFFIX"] = "АФФИКС",
+    ["NEEDED"] = "НЕОБХОДИМО",
+    ["LIMIT"] = "ПРЕДЕЛ",
+    ["GAIN"] = "ПРИРОСТ",
+    ["Not Possible!"] = "Невозможно!",
+
+    -- ui/settings.lua
+    ["Show UI button on minimap"] = "Показать кнопку UI на миникарте",
+    ["Activate M+ Timer"] = "Активировать таймер М+",
+    ["Movable M+ Timer"] = "Передвижной таймер M+",
+    ["M+ Timer Scale"] = "Масштаб M+ таймера",
+    ["M+ Timer Alpha"] = "Прозрачность М+ таймера",
+
+    -- ui/talentCheck.lua
+    ["Talent Check"] = "Проверка талантов",
+    ["Hover over Icons for more Info."] = "Наведите курсор на значки, чтобы получить дополнительную информацию.",
+    ["Reason for Recommendation:"] = "Причина для рекомендации:",
+    ["Affix for Recommendation:"] = "Рекомендации для Аффикса:",
+    ["Recommendation Specs:"] = "Рекомендуемые характеристики:",
+    ["Is talented:"] = "Таланты:",
+    ["It appears that there are no talent recommendations for this affix yet. You might want to submit something through the Discord."] =
+    "Похоже, что для этого Аффикса пока нет рекомендаций по талантам. Возможно, Вы захотите отправить что-нибудь через Discord.",
+
+    -- main.lua
+    ["Click to open an overview of your party's keystones and dungeon items."] =
+    "Нажмите, чтобы открыть обзор ключей в Вашей группе и предметов из подземелий.",
+    ["enabled"] = "включено",
+    ["disabled"] = "выключено",
+    ["Developer mode"] = "Режим разработчика",
+    ["Developer chat"] = "Чат разработчиков",
+    ["Minimap button"] = "Кнопка миникарты",
+    ["You're running an"] = "Вы запускаете",
+    ["build of the addon. Features may be broken or only half finished in alpha versions!"] =
+    "сборка аддона. В альфа-версиях функции могут быть неработающими или готовыми только наполовину!",
+    ["version of the addon. Thank you for helping test, and please report any issues on the Github so they can be fixed before release. :)"] =
+    "версия аддона. Благодарим Вас за помощь в тестировании, и, пожалуйста, сообщайте о любых проблемах на Github, чтобы их можно было исправить до релиза. :)",
+    ["type"] = "тип",
+    ["to open the KeystoneCompanion UI, or click the minimap button."] =
+    "чтобы открыть пользовательский интерфейс KeystoneCompanion или нажмите кнопку на миникартe.",
+}
