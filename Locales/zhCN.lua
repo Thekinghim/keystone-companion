225559206af12bfc951d5ba981139fc78dd341a3
萌丶汉丶纸 <52372535+fredako@users.noreply.github.com>
2024-07-08 23:21:47 +0800

    Add files via upload

---
+++ Locales/zhCN.lua
@@ -0,0 +1,74 @@
+local _, Private = ...
+Private.Locales = Private.Locales or {}
+
+Private.Locales["zhCN"] = {
+    --isEditing = true,
+    -- ui/bestDungeons.lua
+    ["WEEKLY BEST"] = "每周最佳",
+
+    -- ui/dungeonTimer.lua
+    ["M+ Count"] = "大秘境计数",
+
+    -- ui/main.lua
+    ["Player name"] = "玩家名称",
+    ["Food"] = "食物",
+    ["Runes"] = "符文",
+    ["Potions"] = "药水",
+    ["Flasks"] = "瓶剂",
+    ["Weapon"] = "武器",
+    ["Join us on Discord"] = "在 Discord 上加入我们",
+    ["Copy below URL into your browser."] = "将以下 URL 复制到浏览器.",
+    ["Mythic+ score"] = "大秘境分数",
+    ["Item level"] = "物品等级",
+    ["Item level in bags"] = "背包内物品等级",
+    ["You do not have this teleport"] = "你没有这个传送",
+    ["Teleport not usable"] = "无法使用传送",
+    ["Click to teleport"] = "点击传送",
+
+    -- ui/scoreCalculator.lua
+    ["Target Score"] = "目标分数",
+    ["Max. Key Level"] = "最大. 钥匙等级",
+    ["Only this Week"] = "仅本周",
+    ["Calculate"] = "计算",
+    ["NAME"] = "名称",
+    ["LEVEL"] = "等级",
+    ["AFFIX"] = "词缀",
+    ["NEEDED"] = "需要",
+    ["LIMIT"] = "限制",
+    ["GAIN"] = "收益",
+    ["Not Possible!"] = "不可行!",
+
+    -- ui/settings.lua
+    ["Show UI button on minimap"] = "在小地图上显示用户界面按钮",
+    ["Activate M+ Timer"] = "激活大秘境定时器",
+    ["Movable M+ Timer"] = "移动大秘境计时器",
+    ["M+ Timer Scale"] = "大秘境计时器缩放",
+    ["M+ Timer Alpha"] = "大秘境计时器透明度",
+
+    -- ui/talentCheck.lua
+    ["Talent Check"] = "天赋检查",
+    ["Hover over Icons for more Info."] = "悬停在图标上查看更多信息.",
+    ["Reason for Recommendation:"] = "推荐理由:",
+    ["Affix for Recommendation:"] = "推荐词缀:",
+    ["Recommendation Specs:"] = "推荐专精:",
+    ["Is talented:"] = "点出天赋:",
+	["It appears that there are no talent recommendations for this affix yet. You might want to submit something through the Discord."] =
+    "目前似乎还没有关于这个词缀的天赋推荐. 你可能需要通过 Discord 提交内容.",
+
+    -- main.lua
+    ["Click to open an overview of your party's keystones and dungeon items."] =
+    "点击可打开队伍的钥石和地下城物品概览.",
+    ["enabled"] = "启用",
+    ["disabled"] = "禁用",
+    ["Developer mode"] = "开发者模式",
+    ["Developer chat"] = "开发者对话",
+    ["Minimap button"] = "小地图按钮",
+    ["You're running an"] = "你正在进行的",
+    ["build of the addon. Features may be broken or only half finished in alpha versions!"] =
+    "插件版本. 在 alpha 版本中功能可能被破坏或只完成了一半!",
+    ["version of the addon. Thank you for helping test, and please report any issues on the Github so they can be fixed before release. :)"] =
+    "插件版本. 感谢你帮助测试, 如有任何问题请在 Github 上报告, 以便在发布前修复. :)",
+    ["type"] = "类型",
+    ["to open the KeystoneCompanion UI, or click the minimap button."] =
+    "打开 KeystoneCompanion 用户界面或点击小地图按钮.",
+}
